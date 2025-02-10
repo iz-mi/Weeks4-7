e66d27f09930f26fa3fa16fc0650296f07c55b6c
iz-mi <wangmic@sheridancollege.ca>
2025-02-09 20:21:55 -0500

    updated visuals

--- Assets/Assignment2/Scripts/spawner.cs
+++ Assets/Assignment2/Scripts/spawner.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class spawner : MonoBehaviour
 {
     public GameObject bubblePrefab;
+    public Vector2 spawnXY = new Vector2(10f, 10f);
+
+    //float spawnX = Random.Range();
+    //float spawnY = Random.Range();
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +20,8 @@ public class spawner : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            //spawnX = Math.Random()
+            //spawnY = Math.Random()
             Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Instantiate(bubblePrefab, mouse, Quaternion.identity);
             //Instantiate(bubblePrefab);
