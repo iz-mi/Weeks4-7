66392b5742461bd86540f16de74c0a017e539051
iz-mi <wangmic@sheridancollege.ca>
2025-02-17 09:49:24 -0500

    scene update to destroy instantiate at runtime

--- Assets/Assignment2/Scripts/spawner.cs
+++ Assets/Assignment2/Scripts/spawner.cs
@@ -13,7 +13,7 @@ public class spawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+            
     }
 
     // Update is called once per frame
